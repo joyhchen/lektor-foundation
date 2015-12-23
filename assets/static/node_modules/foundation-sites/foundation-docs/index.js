b0d1d0bab52462ea50120ac186e172fd2eec3103
Joy <joythewizard@gmail.com>
2015-12-23 14:13:04 -0600

    remove folder

--- assets/static/node_modules/foundation-sites/foundation-docs/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  buildSearch: require('./lib/buildSearch'),
-  handlebars: require('./lib/handlebars'),
-  marked: require('./lib/marked'),
-  componentTemplate: 'node_modules/foundation-docs/templates/component.html'
-}
